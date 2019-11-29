e43c510b7e8f9d7407804256ec7b9f15f1d8a2d9
Giancarlo <gmatute@growthaccelerationpartners.com>
2019-11-29 15:05:41 -0600

    change mysql script

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -3,18 +3,15 @@ VERSION=5.6.46
 OS=linux-glibc2.12
 ARCH=x86_64
 
-#cd /tmp && { wget $MYSQL_URL ; cd -; }
+cd /tmp && { wget $MYSQL_URL ; cd -; }
 
-#groupadd mysql
-#useradd -r -g mysql -s /bin/false mysql
+groupadd mysql
+useradd -r -g mysql -s /bin/false mysql
 cd /usr/local
-#tar xvf "/tmp/mysql-$VERSION-$OS-$ARCH.tar.gz"
+tar xvf "/tmp/mysql-$VERSION-$OS-$ARCH.tar.gz"
 ln -s "mysql-$VERSION-$OS-$ARCH" mysql
 export PATH=$PATH:/usr/local/mysql/bin
 cd mysql
-mkdir mysql-files
-chown mysql:mysql mysql-files
-chmod 750 mysql-files
-bin/mysqld --initialize --user=mysql
-bin/mysql_ssl_rsa_setup
+scripts/mysql_install_db --user=mysql
 bin/mysqld_safe --user=mysql &
+cp support-files/mysql.server /etc/init.d/mysql.server
